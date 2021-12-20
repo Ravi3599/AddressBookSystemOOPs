0b19d396b6bfe289b7931d9da776bea6bf5fe8cc
Ravi3599 <94590162+Ravi3599@users.noreply.github.com>
2021-12-20 15:16:46 +0530

    MODIFIED: some space changes

--- src/com/bridgelabz/addressbook/Person.java
+++ src/com/bridgelabz/addressbook/Person.java
@@ -9,17 +9,18 @@ public class Person {
 	static int zip;
 	static long phoneNumber;
 	static int j=0;
-    static Contact [] personDetails = new Contact[10];
+    	static Contact [] personDetails = new Contact[10];
 
 	
 	public static void main(String args[]) {
 		
-		Contact person1 = new Contact("Raj","Kumar","Nagar","MH",35,8620,"raj@gmail.com");
-		Contact person2 = new Contact("Shashi","Rao","Chowk","TD",98,9736,"shashi@yahoo.com");
-		Contact person3 = new Contact("Apurva","Mehta","Mahal","AP",47,7620,"amar@edu.com");
+	    Contact person1 = new Contact("Raj","Kumar","Nagar","MH",35,8620,"raj@gmail.com");
+	    Contact person2 = new Contact("Shashi","Rao","Chowk","TD",98,9736,"shashi@yahoo.com");
+	    Contact person3 = new Contact("Apurva","Mehta","Mahal","AP",47,7620,"amar@edu.com");
 	    personDetails[0]=person1;
 	    personDetails[1]=person2;
 	    personDetails[2]=person3;
+		
 	    System.out.println("Created contact list is");
 	    for(int i = 0; i < 3;i++) {
 		   System.out.println(personDetails[i]);
