42e51342cdd030b8b081d3b2782d9d6cee594891
Ravi3599 <ravikantdukre2018@gmail.com>
2021-12-20 14:13:51 +0530

    UPDATED: created a contact list

--- src/com/bridgelabz/addressbook/Person.java
+++ src/com/bridgelabz/addressbook/Person.java
@@ -1,9 +1,22 @@
 package com.bridgelabz.addressbook;
 
+import java.util.Scanner;
+
 public class Person {
+	static Contact [] personDetails = new Contact[10];
 	
 	public static void main(String args[]) {
 		
-		System.out.println("Welcome to Address Book System Program");
+		Contact person1 = new Contact("Raj","Kumar","Nagar","MH",35,8620,"raj@gmail.com");
+		Contact person2 = new Contact("Shashi","Rao","Chowk","TD",98,9736,"shashi@yahoo.com");
+		Contact person3 = new Contact("Apurva","Mehta","Mahal","AP",47,7620,"amar@edu.com");
+	    personDetails[0]=person1;
+	    personDetails[1]=person2;
+	    personDetails[2]=person3;
+	    
+	    System.out.println("Created contact list is");
+	    for(int i = 0; i < 3;i++) {
+		   System.out.println(personDetails[i]);
+	    }
 	}
 }
