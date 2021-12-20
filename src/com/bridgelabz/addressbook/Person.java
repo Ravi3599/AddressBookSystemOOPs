f27d8cf1c05a5fdf106edcf87d4c01ee91001bcd
Ravi3599 <ravikantdukre2018@gmail.com>
2021-12-20 14:09:36 +0530

    ADDED: welcome message

---
+++ src/com/bridgelabz/addressbook/Person.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbook;
+
+public class Person {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Welcome to Address Book System Program");
+	}
+}
